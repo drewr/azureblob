3c7c70e8ec51e7e6c4912af4352d33e25945eca5
Andrew Raines <andrew@raines.me>
2014-03-03 10:24:23 -0600

    Remove key

--- src/azureblob/main.clj
+++ src/azureblob/main.clj
@@ -3,12 +3,6 @@
   (:require [clojure.tools.cli :as cli]
             [azureblob.core :as azure]))
 
-   "es1"
-   (str "fwabTQeFvQb9/45oklR9B"
-        "AH4k7e1WsU7hYZJbFoL"
-        "CdCMF6BCsE/85/WqlEbYBj4B"
-        "kEj8+m9Z5Nrr9Lvob1zIaQ==")
-
 (def opts
   [["-n" "--name NAME" "Account name"]
    ["-k" "--key KEY" "Account key"]
